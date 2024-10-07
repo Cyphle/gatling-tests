19ce4c8da8489cf6dcc4292b91e686d983f1f372
Cyphle <cyril.phamle@gmail.com>
2024-10-07 09:55:54 +0200

    conf

--- src/test/java/fr/cyphle/utils/OAuthConfig.java
+++ src/test/java/fr/cyphle/utils/OAuthConfig.java
@@ -1,18 +1,18 @@
 package fr.cyphle.utils;
 
 public class OAuthConfig {
-    public static final String BASE_URL = "https://platform.stonal-test.io";
+    public static final String BASE_URL = "https://baseurl.com";
 
-    public static final String OIDC = "https://sso.stonal-test.io";
-    public static final String AUTHORIZE_URL = OIDC + "/realms/stonal/protocol/openid-connect/auth";
-    public static final String TOKEN_URL = OIDC + "/realms/stonal/protocol/openid-connect/token";
+    public static final String OIDC = "https://openid.server.com";
+    public static final String AUTHORIZE_URL = OIDC + "/realms/myrealm/protocol/openid-connect/auth";
+    public static final String TOKEN_URL = OIDC + "/realms/myrealm/protocol/openid-connect/token";
 
-    public static final String PASSWORD_FLOW_CLIENT_ID = "powerbi-password-flow";
-    public static final String PASSWORD_FLOW_CLIENT_SECRET = "D7gsFJbYgQ5cs8mqYi7J5YPTW8cSFIyD";
-    public static final String CODE_FLOW_CLIENT_ID = "api-gateway";
-    public static final String CODE_FLOW_CLIENT_SECRET = "D7gsFJbYgQ5cs8mqYi7J5YPTW8cSFIyD";
+    public static final String PASSWORD_FLOW_CLIENT_ID = "client_id_password_flow";
+    public static final String PASSWORD_FLOW_CLIENT_SECRET = "client_secret_password_flow";
+    public static final String CODE_FLOW_CLIENT_ID = "client_id_code_flow";
+    public static final String CODE_FLOW_CLIENT_SECRET = "client_secret_code_flow";
     public static final String SCOPE = "openid";
 
-    public static final String USERNAME = "admin-plateforme@lafoncierenumerique.com";
-    public static final String PASSWORD = "n2C.v947RN%_UlTYWLqKmX@GPj[yVu8h";
+    public static final String USERNAME = "johndoe@gatling.test";
+    public static final String PASSWORD = "passpass";
 }
